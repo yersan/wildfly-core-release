0b236ca7a609fcdb7b2f994562b0902723a0494d
Paul Ferraro <paul.ferraro@redhat.com>
2023-08-23 19:43:25 -0400

    Release script fails to commit/tag if git is configured to sign commits.

--- run-docker-release.sh
+++ run-docker-release.sh
@@ -8,5 +8,6 @@ docker run \
 	-v wfcore-release-checkouts:/home/wfcore/checkouts \
 	-v ~/.gitconfig:/home/wfcore/.gitconfig \
 	-v ~/.ssh:/home/wfcore/.ssh \
+    -v ~/.gnupg:/home/wfcore/.gnupg \
 	-it "${USER}/wildfly-core-release:11" \
 	/home/wfcore/do-release.sh $1 $2 $3 $4 $5 $6 $7 $8
--- run-docker-shell.sh
+++ run-docker-shell.sh
@@ -7,5 +7,6 @@ docker run \
 	-v wfcore-release-checkouts:/home/wfcore/checkouts \
 	-v ~/.gitconfig:/home/wfcore/.gitconfig \
 	-v ~/.ssh:/home/wfcore/.ssh \
+    -v ~/.gnupg:/home/wfcore/.gnupg \
 	-it ${USER}/wildfly-core-release:11 \
   bash
